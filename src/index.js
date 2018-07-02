20e0e4fef99f61917c641b14dafe8a64ccef3a7a
Alex Mercier <pro.alexandre.mercier@gmail.com>
2018-07-02 21:32:21 +0200

    Added API documentation

--- src/index.js
+++ src/index.js
@@ -8,8 +8,22 @@ import { dirname } from 'path';
 import { stringify } from 'purdy';
 import stripAnsi from 'strip-ansi';
 
+/**
+ * Add characters at the begenning of every line of a multi-line string.
+ *
+ * @param {string} string - Input string.
+ * @param {number} length - Number of characters to insert at every line.
+ * @param {string} [char=' '] - Character to insert.
+ * @returns {string} The indented string.
+ */
 export const indent = (string, length, char = ' ') => `${string.replace(/\n/g, `\n${char.repeat(length)}`)}`;
 
+/**
+ * Transform an error into a user-readable, colored string.
+ *
+ * @param  {Error} error - Input error.
+ * @returns {string} The formatted message.
+ */
 export function formatError(error) {
   const { message, stack } = separateMessageFromStack(error.stack);
   const stackTrace = formatStackTrace(
@@ -20,11 +34,24 @@ export function formatError(error) {
   return `${red(message)}\n${stackTrace.replace(/\n {4}/g, '\n')}\n`;
 }
 
+/**
+ * Transform an object into a user-readable, colored string. If the resulting
+ * string is shorter than 200 characters, inlines it.
+ *
+ * @param {Object} object - The input object.
+ * @returns {string} A stringified version of the object.
+ */
 function formatObject(object) {
   const str = stringify(object, { indent: 2, arrayIndex: false });
   return grey(stripAnsi(str).length < 200 ? str.replace(/\n */g, ' ') : `\n${str}\n`);
 }
 
+/**
+ * Transform a value of any type into a user-readable, colored string.
+ *
+ * @param {*} value - A value of any type.
+ * @returns {string} A stringified version of the value.
+ */
 function format(value) {
   if (value instanceof Error) {
     return formatError(value);
@@ -32,6 +59,32 @@ function format(value) {
   return typeof value === 'string' ? value : formatObject(value);
 }
 
+/**
+ * @typedef {Object} Log~options
+ * @property {string[]} levels - Log levels.
+ * @property {function[]} colors - Color function for each level.
+ * @property {boolean[]} inverse - Whether to inverse color for each level.
+ * @property {string[]} labels - Label of each level.
+ * @property {string} separator - Separator between time and label, and between header and body
+ * @property {Object|Log~options~streamFn} stream - Stream or function that returns a Stream.
+ * @property {boolean} displayTime - Whether to display time.
+ * @property {string} displayTimeFormat - Format to display time in. See [dateformat].
+ * @property {string} level - All log calls below this level will be ignored.
+ *
+ * [dateformat]: https://www.npmjs.com/package/dateformat
+ */
+
+/**
+ * Function that returns a Stream
+ * @callback Log~options~streamFn
+ * @param {number} level - Level number.
+ * @returns Stream Returns a Stream
+ */
+
+/**
+ * Default configuration
+ * @type {Log~options}
+ */
 export const defaultOptions = {
   levels: ['emergency', 'alert', 'critical', 'error', 'warning', 'notice', 'info', 'debug'],
   colors: [red.bold, red, red.bold, red, magenta, yellow, blue, grey],
@@ -45,9 +98,24 @@ export const defaultOptions = {
   level: 'info',
 };
 
-const getStream = (level, { stream }) => (typeof stream === 'function' ? stream(level) : stream);
+/**
+ * If an option is a function, apply it with given parameters, otherwise returns it.
+ *
+ * @param {Function|*} option - The option bvalue or function.
+ * @param {*[]} args - Arguments to call `option` with, if `option` is a function.
+ * @returns {*} The return of `option(...args)`, or `option`.
+ */
+const applyOption = (option, ...args) => (typeof option === 'function' ? option(...args) : option);
 
+/**
+ * Fancy lightweight logging utility.
+ */
 export class Log {
+  /**
+   * Creates an instance of `Log`.
+   *
+   * @param {Log~options} [options={}] - Options.
+   */
   constructor(options = {}) {
     this.options = defaults({}, options, defaultOptions);
     this.options.levels.forEach((name, logLevel) => {
@@ -55,10 +123,19 @@ export class Log {
     });
   }
 
+  /**
+   * @property {number} level - Integer value of `this.options.level`.
+   */
   get level() {
     return this.options.levels.indexOf(this.options.level);
   }
 
+  /**
+   * Build the header.
+   *
+   * @param {number} level - Level number.
+   * @returns {string} The constructed header.
+   */
   buildHeader(level) {
     const {
       labels, colors, inverse, displayTime, displayTimeFormat, separator,
@@ -70,25 +147,65 @@ export class Log {
     return `${time && grey(`${time}${separator}`)}${labelColor(label)}${color(separator)}`;
   }
 
+  /**
+   * Build the body.
+   *
+   * @param {*[]} parts - Parts of the message, not formatted.
+   * @returns {string} The constructed body.
+   */
   buildBody(parts) {
     const messages = parts.map(this.options.format);
     return messages.join(messages.some(s => s.includes('\n')) ? '\n' : ' ');
   }
 
+  /**
+   * Build the entire message.
+   *
+   * @param {number} level - Level number.
+   * @param {*[]} parts - Parts of the message, not formatted.
+   * @returns {string} The constructed message.
+   */
   buildMessage(level, parts) {
     const header = this.buildHeader(level);
     const body = this.buildBody(parts);
     return `${header}${indent(body, stripAnsi(header).length)}\n`;
   }
 
+  /**
+   * Build a message and write it to the stream.
+   *
+   * @param {number} level - Level number.
+   * @param {*[]} parts - Parts of the message, not formatted.
+   * @returns {undefined} Nothing.
+   */
   write(level, parts) {
     if (level > this.level) {
       return;
     }
-    getStream(level, this.options).write(this.buildMessage(level, parts));
+    const stream = applyOption(this.options.stream, [level]);
+    stream.write(this.buildMessage(level, parts));
   }
 }
 
+/**
+ * Utility function that will create a new instance of `Log` with options taken
+ * from environment variables.
+ *
+ * Variables:
+ * - `NODE_ENV`:
+ *   - `'development'` (default): `displayTime` is disabled,
+ *   - `'test'`: `displayTime` is disabled, `'level'` is set to `'critical'`,
+ *   - `'production'`: Use default values for each option.
+ * - `LOG`: Sets `level` value.
+ * - `LOG_LEVEL`: Alias for `LOG`.
+ * - `LOG_TIME`: When set to `true` or `1`, enables `displayTime`.
+ * - `LOG_TIME_FORMAT`: Sets `displayTimeFormat` value.
+ *
+ * @param {Object} [options={}] - Additional options. Will take precedence over other ones.
+ * @param {Object} [env=process.env || {}] - `env` object.
+ * @param {string} [environment=env.NODE_ENV || 'development'] - Environment.
+ * @returns {Log} A new instance of `Log`.
+ */
 export function fromEnv(options = {}, env = process.env || {}, environment = env.NODE_ENV || 'development') {
   const mergedOptions = defaults(
     options,
